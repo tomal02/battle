b67a024634400c1786f298a055a4515af8e2feef
Tom Lee <tomalee2002@gmail.com>
2021-09-09 00:12:16 +0100

    first commit

---
+++ spec/features/player_names_spec.rb
@@ -0,0 +1,9 @@
+feature 'Player names' do
+  scenario 'Submit player names' do
+    visit '/'
+    fill_in :player1_name, with: 'Tim'
+    fill_in :player2_name, with: 'Bob'
+    click_button 'Submit'
+    expect(page).to have_content 'Tim vs Bob'
+  end
+end
