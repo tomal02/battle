5c07c80efb41bf2b4f9c4931a3bdffd39a76b483
Tom Lee <tomalee2002@gmail.com>
2021-09-10 12:09:05 +0100

    adds test spec for player 2 hit points

---
+++ spec/features/hit_points_spec.rb
@@ -0,0 +1,10 @@
+feature 'View hit points' do
+  scenario 'Can see player 2 hit points' do
+    visit '/'
+    fill_in :player1_name, with: 'Tim'
+    fill_in :player2_name, with: 'Bob'
+    click_button 'Submit'
+
+    expect(page).to have_content "Bob: 100hp"
+  end
+end
