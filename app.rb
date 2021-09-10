2db90c6b25dbe30b0aafcc7906b3ca6b70d31329
Tom Lee <tomalee2002@gmail.com>
2021-09-10 17:59:29 +0100

    adds attack functionality for player 1

--- app.rb
+++ app.rb
@@ -24,5 +24,11 @@ class Battle < Sinatra::Base
     erb :play
   end
 
+  get '/attack' do
+    @player1_name = session[:player1_name]
+    @player2_name = session[:player2_name]
+    erb :attack
+  end
+
   run! if app_file == $0
 end
